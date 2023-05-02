04a75ecd4ab2441963b5b6abe9d749e62bc3efb5
Jacob Riley <Vidkol18@gmail.com>
2023-05-02 22:36:07 +0100

    Added all the basics to get it somewhat looking good.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,6 +6,6 @@ import { Ingredient } from '../shared/ingredient.model';
     templateUrl: './shopping-list.component.html',
     styleUrls: ['./shopping-list.component.css'],
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent {
     ingredeients: Ingredient[] = [new Ingredient('Bread', 2), new Ingredient('Cheese', 1), new Ingredient('Milk', 2), new Ingredient('Eggs', 12), new Ingredient('Butter', 1), new Ingredient('Sugar', 1), new Ingredient('Water', 12)];
 }
